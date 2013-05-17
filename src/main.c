46a91f1aa7c11eb3569820c847a1f634a4afe4d5
Mark Suman <mwsuman@gmail.com>
2013-05-17 14:29:53 -0600

    BJ C fu.

--- src/main.c
+++ src/main.c
@@ -7,7 +7,6 @@
 
 #define BITMAP_BUFFER_BYTES 1024
 
-
 #define MY_UUID { 0x8D, 0x22, 0x23, 0x1C, 0xC1, 0x7F, 0x43, 0x29, 0xAF, 0xF5, 0x87, 0x7F, 0x30, 0xBF, 0x65, 0xA0 }
 PBL_APP_INFO(MY_UUID,
              "Quiz Metronome", "Instructure",
@@ -31,6 +30,13 @@ enum {
   TIMER_VALUE_KEY = 0x1,  // TUPLE_CSTRING
 };
 
+static uint32_t TIMER_ICONS[] = {
+  // RESOURCE_ID_IMAGE_SUN,
+  // RESOURCE_ID_IMAGE_CLOUD,
+  // RESOURCE_ID_IMAGE_RAIN,
+  // RESOURCE_ID_IMAGE_SNOW
+};
+
 // TODO: Error handling
 static void sync_error_callback(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
   (void) dict_error;
@@ -42,10 +48,11 @@ static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tup
   (void) old_tuple;
 
   switch (key) {
-  // case TIMER_ICON_KEY:
-  //   load_bitmap(WEATHER_ICONS[new_tuple->value->uint8]);
-  //   layer_mark_dirty(&s_data.icon_layer.layer);
-  //   break;
+  case TIMER_ICON_KEY:
+    // todo: update the image
+    //load_bitmap(TIMER_ICONS[new_tuple->value->uint8]);
+    //layer_mark_dirty(&s_data.icon_layer.layer);
+    break;
   case TIMER_VALUE_KEY:
     strncpy(s_data.timerValue, new_tuple->value->cstring, 16);
     layer_mark_dirty(&s_data.timer_layer.layer);
@@ -70,9 +77,28 @@ void quiz_metronome_init(AppContextRef ctx) {
   text_layer_set_text(&s_data.timer_layer, s_data.timerValue);
   layer_add_child(&window->layer, &s_data.timer_layer.layer);
 
+
+  char bytes[16] = MY_UUID;
+  char byteString[36] = "0x";
+
+  int byteOffset = 2;
+  for (int i=0; i<16; ++i) {
+
+    char byte = bytes[i];
+    char val = '0'+(byte>>4);
+    if (val > '9') val += 8;
+    byteString[byteOffset] = val;
+    val = '0'+(byte & 0x0f);
+    if (val > '9') val += 8;
+    byteString[byteOffset+1] = val;
+
+    byteOffset += 2;
+  }
+
   Tuplet initial_values[] = {
-    //TupletInteger(WEATHER_ICON_KEY, (uint8_t) 1),
-    TupletCString(TIMER_VALUE_KEY, "12:34:56"),
+
+    TupletInteger(TIMER_ICON_KEY, (uint8_t) 1),
+    TupletCString(TIMER_VALUE_KEY, (const char *)byteString),
   };
 
   app_sync_init(&s_data.sync, s_data.sync_buffer, sizeof(s_data.sync_buffer), initial_values, ARRAY_LENGTH(initial_values),
