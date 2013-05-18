d9f5e426436268eb2088dec1fb7726500deb636d
Mark Suman <mwsuman@gmail.com>
2013-05-17 19:34:41 -0600

    Currently rejects messages sent from the iOS app.

--- src/main.c
+++ src/main.c
@@ -7,12 +7,13 @@
 
 #define BITMAP_BUFFER_BYTES 1024
 
-#define MY_UUID { 0x8D, 0x22, 0x23, 0x1C, 0xC1, 0x7F, 0x43, 0x29, 0xAF, 0xF5, 0x87, 0x7F, 0x30, 0xBF, 0x65, 0xA0 }
-PBL_APP_INFO(MY_UUID,
-             "Quiz Metronome", "Instructure",
-             1, 0, /* App version */
-             DEFAULT_MENU_ICON,
-             APP_INFO_STANDARD_APP);
+
+#define MY_UUID {0x8D, 0x22, 0x23, 0x1C, 0xC1, 0x7F, 0x43, 0x29, 0xAF, 0xF5, 0x87, 0x7F, 0x30, 0xBF, 0x65, 0xA0}
+PBL_APP_INFO(MY_UUID, "Quiz Metronome", "Instructure", 0x1, 0x0, DEFAULT_MENU_ICON, APP_INFO_STANDARD_APP);
+// #define MY_UUID {0x42, 0xc8, 0x6e, 0xa4, 0x1c, 0x3e, 0x4a, 0x7, 0xb8, 0x89, 0x2c, 0xcc, 0xca, 0x91, 0x41, 0x98}
+// PBL_APP_INFO(MY_UUID, "Pebble Weather", "Pebble Technology", 0x1, 0x0, DEFAULT_MENU_ICON, APP_INFO_STANDARD_APP);
+
+//{0x8D, 0x22, 0x23, 0x1C, 0xC1, 0x7F, 0x43, 0x29, 0xAF, 0xF5, 0x87, 0x7F, 0x30, 0xBF, 0x65, 0xA0}
 
 static struct QuizData {
   Window window;
@@ -26,8 +27,8 @@ static struct QuizData {
 } s_data;
 
 enum {
-  TIMER_ICON_KEY = 0x0,         // TUPLE_INT
-  TIMER_VALUE_KEY = 0x1,  // TUPLE_CSTRING
+  //TIMER_ICON_KEY = 0x0,         // TUPLE_INT
+  TIMER_VALUE_KEY = 0x0,  // TUPLE_CSTRING
 };
 
 static uint32_t TIMER_ICONS[] = {
@@ -48,11 +49,11 @@ static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tup
   (void) old_tuple;
 
   switch (key) {
-  case TIMER_ICON_KEY:
+  // case TIMER_ICON_KEY:
     // todo: update the image
     //load_bitmap(TIMER_ICONS[new_tuple->value->uint8]);
     //layer_mark_dirty(&s_data.icon_layer.layer);
-    break;
+  //   break;
   case TIMER_VALUE_KEY:
     strncpy(s_data.timerValue, new_tuple->value->cstring, 16);
     layer_mark_dirty(&s_data.timer_layer.layer);
@@ -77,28 +78,9 @@ void quiz_metronome_init(AppContextRef ctx) {
   text_layer_set_text(&s_data.timer_layer, s_data.timerValue);
   layer_add_child(&window->layer, &s_data.timer_layer.layer);
 
-
-  char bytes[16] = MY_UUID;
-  char byteString[36] = "0x";
-
-  int byteOffset = 2;
-  for (int i=0; i<16; ++i) {
-
-    char byte = bytes[i];
-    char val = '0'+(byte>>4);
-    if (val > '9') val += 8;
-    byteString[byteOffset] = val;
-    val = '0'+(byte & 0x0f);
-    if (val > '9') val += 8;
-    byteString[byteOffset+1] = val;
-
-    byteOffset += 2;
-  }
-
   Tuplet initial_values[] = {
-
-    TupletInteger(TIMER_ICON_KEY, (uint8_t) 1),
-    TupletCString(TIMER_VALUE_KEY, (const char *)byteString),
+    //TupletInteger(TIMER_ICON_KEY, (uint8_t) 1),
+    TupletCString(TIMER_VALUE_KEY, "12:34:56"),
   };
 
   app_sync_init(&s_data.sync, s_data.sync_buffer, sizeof(s_data.sync_buffer), initial_values, ARRAY_LENGTH(initial_values),
